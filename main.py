f5be6e2b99302c09d888e739fedbafb1602e4f8c
HassanElwany <elwany_hassan@hotmail.com>
2023-11-18 13:30:14 +0300

    add moving to the ball

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ screen.tracer(0)
 
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
-ball = Ball((0, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
@@ -24,5 +24,14 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+
+    # detecting ball collision with up and down wall
+    if ball.ycor() > 300 or ball.ycor() < -300:
+        # need to change directions of ball
+        ball.bounce_y()
+
+    if (ball.distance(right_paddle) < 50 and ball.xcor() > 340) or (ball.distance(left_paddle) < 50 and ball.xcor() < - 340) :
+        ball.bounce_x()
+        print("match")
 
 screen.exitonclick()
